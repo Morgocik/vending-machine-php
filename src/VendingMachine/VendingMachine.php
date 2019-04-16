3b9a833f33a804381e6b310bf85c0b7df308a4fb
Morgocik <github@danilowicz.info>
2019-04-16 20:24:41 +0200

    first

--- src/VendingMachine/VendingMachine.php
+++ src/VendingMachine/VendingMachine.php
@@ -1,14 +1,45 @@
 <?php
 
-declare(strict_types=1);
-
 namespace VendingMachine;
 
-final class VendingMachine {
-    function __construct() {}
+class VendingMachine
+{
+    const NICKEL = 'N';
+    const DIME = 'D';
+    const QUARTER = 'Q';
+    const DOLLAR = 'S';
 
-    public function service(): string
+    const ACCEPT_MONEY = [
+        self::NICKEL,
+        self::DIME,
+        self::QUARTER,
+        self::DOLLAR
+        ];
+
+    private $coins = [];
+
+    public function countCoins()
     {
-        return 'no service';
+        return count($this->coins);
+    }
+
+    public function isCorectMoney(string $money)
+    {
+
+        return in_array(strtoupper($money), self::ACCEPT_MONEY);
+    }
+
+    public function addMoney(string $money)
+    {
+        if ( $this->isCorectMoney($money)) {
+            $this->coins[] = $money;
+            return true;
+        }
+        return false;
+    }
+
+    public function returnMoney()
+    {
+        return $this->coins;
     }
 }
--- tests/VendingMachine/VendingMachineTest.php
+++ tests/VendingMachine/VendingMachineTest.php
@@ -1,26 +1,69 @@
 <?php
 
-declare(strict_types=1);
 
 namespace VendingMachine;
 
+
+use VendingMachine\VendingMachine;
 use PHPUnit\Framework\TestCase;
 
-final class VendingMachineTest extends TestCase
+class VendingMachineTest extends TestCase
 {
-    /**
-     * @var VendingMachine
-     */
-    private $vendingMachine;
-
-    public function setUp(): void
+    public function setUp()
     {
-        $vm = new VendingMachine();
-        $this->vendingMachine = $vm;
+        $this->vendingMachine = new VendingMachine();
     }
 
-    public function testServiceReturnsNoService(): void
+    public function testCountCoins()
     {
-        $this->assertEquals('service', $this->vendingMachine->service());
+
+        $this->assertEquals(0,  $this->vendingMachine->countCoins());
+    }
+
+    public function testIsCorectMoney()
+    {
+        $this->assertEquals( true , $this->vendingMachine->isCorectMoney('q'));
+
+    }
+
+    public function testAddMoney()
+    {
+        $this->assertEquals(true,  $this->vendingMachine->addMoney('s'));
+    }
+
+    public function testReturnMoney()
+    {
+        $this->vendingMachine->addMoney('s');
+        $this->vendingMachine->addMoney('q');
+        $this->assertEquals(['s','q'],  $this->vendingMachine->returnMoney());
+    }
+
+    public function testIsCoinsTwo()
+    {
+        $this->vendingMachine->addMoney('s');
+        $this->vendingMachine->addMoney('q');
+        $this->assertEquals(2,  $this->vendingMachine->countCoins());
+    }
+
+    /**
+     * @dataProvider coinsProvider
+     * @param $inputCoins
+     * @param $expectedCount
+     */
+    public function testManyCoins($inputCoins, $expectedCount)
+    {
+        foreach ($inputCoins as $coin) {
+            $this->vendingMachine->addMoney($coin);
+        }
+        $this->assertEquals($expectedCount,  $this->vendingMachine->countCoins());
+    }
+
+    public function coinsProvider()
+    {
+        return [
+            [['s'], 1],
+            [['s', 's'], 2],
+            [['s', 's', 's'], 3],
+        ];
     }
 }
